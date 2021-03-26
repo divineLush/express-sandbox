ba312b7c89de19d1711718c18c171de2590687da
divineLush <homeoflucifer@gmail.com>
2021-03-26 12:24:53 +0700

    static files and middleware

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+// map a route to files
+// express.static is a middleware
+app.use('/assets', express.static(__dirname + '/public'));
+
+// custom middleware
+app.use('/', function(req, res, next) {
+    console.log(`Request url: ${req.url}`);
+    console.log(`Status code: ${res.statusCode}`);
+    next();
+});
+
 // http method that responds to http get request
 app.get('/', function(req, res) {
     // don't have to specify a mime type
     // as express takes care about it
-    res.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+    res.send('<html><head><link rel="stylesheet" type="text/css" href="assets/style.css"></link></head><body><h1>Hello world!</h1></body></html>');
 });
 
 app.get('/person/:id', function(req, res) {
