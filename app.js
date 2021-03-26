8d011d80e6cfbf82dc27d7efac837e4b0fe4a8be
divineLush <homeoflucifer@gmail.com>
2021-03-26 13:29:32 +0700

    ejs template engine

--- app.js
+++ app.js
@@ -14,15 +14,21 @@ app.use('/', function(req, res, next) {
     next();
 });
 
+app.set('view engine', 'ejs');
+
 // http method that responds to http get request
 app.get('/', function(req, res) {
     // don't have to specify a mime type
     // as express takes care about it
-    res.send('<html><head><link rel="stylesheet" type="text/css" href="assets/style.css"></link></head><body><h1>Hello world!</h1></body></html>');
+    // res.send('<html><head><link rel="stylesheet" type="text/css" href="assets/style.css"></link></head><body><h1>Hello world!</h1></body></html>');
+
+    res.render('index');
 });
 
 app.get('/person/:id', function(req, res) {
-    res.send(`<html><head></head><body><h1>Person ID: ${ req.params.id }</h1></body></html>`);
+    // res.send(`<html><head></head><body><h1>Person ID: ${ req.params.id }</h1></body></html>`);
+
+    res.render('person', { id: req.params.id });
 });
 
 app.get('/api', function(req, res) {
