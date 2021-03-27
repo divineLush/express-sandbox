d70efe7c16ace3b9458472f7868b52346f35aa44
divineLush <homeoflucifer@gmail.com>
2021-03-27 23:30:15 +0700

    using bodyparser & post parameters

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var urlEncodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
 
 // map a route to files
 // express.static is a middleware
@@ -28,7 +32,24 @@ app.get('/', function(req, res) {
 app.get('/person/:id', function(req, res) {
     // res.send(`<html><head></head><body><h1>Person ID: ${ req.params.id }</h1></body></html>`);
 
-    res.render('person', { id: req.params.id });
+    res.render('person', {
+        id: req.params.id,
+        queryString: req.query.qstr,
+    });
+});
+
+app.post('/person', urlEncodedParser, function(req, res) {
+    res.send('Thank you!');
+    console.log('/person');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
+});
+
+app.post('/personjson', jsonParser, function(req, res) {
+    res.send('Thank you for the JSON data!');
+    console.log('/personjson');
+    console.log(req.body.firstName);
+    console.log(req.body.lastName);
 });
 
 app.get('/api', function(req, res) {
